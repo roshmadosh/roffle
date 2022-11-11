cd9280e74428904b64389c544fed5cdbd7738dc0
roshmadosh <hiroshi.nobuoka@gmail.com>
2022-11-11 12:51:44 -0500

    refactor index.js for easier keypress control flow

--- static/index.js
+++ static/index.js
@@ -1,4 +1,5 @@
-let contentEditable = document.getElementById("line1")
+const textEditor = document.getElementById("text-editor");
+let contentEditable = document.getElementById("line1");
 const placeholder = document.getElementById("placeholder");
 
 
@@ -6,7 +7,7 @@ let line_number = 1;
 
 contentEditable.addEventListener("focusin", contentEditableEventHandler);
 contentEditable.addEventListener("focusout", contentEditableEventHandler);
-contentEditable.addEventListener("keydown", contentEditableEventHandler);
+contentEditable.addEventListener("keydown", keydownEventHandler);
 
 
 function contentEditableEventHandler(e) {
@@ -21,21 +22,36 @@ function contentEditableEventHandler(e) {
                 placeholder.classList.remove('invisible')
             } break;
 
-        case 'keydown':
-            const textEditor = document.getElementById('text-editor');
-            const max_width = textEditor.offsetWidth * .87;
-
-            if (contentEditable.offsetWidth > max_width || e.key === 'Enter') {
-                e.preventDefault();
-                line_number++
-                textEditor.innerHTML += `<br><span contentEditable="true" id="line${line_number}"></span>`
-                contentEditable = document.getElementById(`line${line_number}`);
-                contentEditable.addEventListener("keydown", contentEditableEventHandler);
-                contentEditable.focus()
-            }
-
-
         default:
             break;
         }
+}
+
+function keydownEventHandler(e) {
+    const textEditor = document.getElementById('text-editor');
+    const max_width = textEditor.offsetWidth * .87;
+    if (contentEditable.offsetWidth > max_width) { 
+        e.preventDefault();
+        createNewLine(); 
+    }
+
+    const key = e.key;
+    switch (key) {
+        case 'Enter':
+            createNewLine();
+            break;
+
+        default:
+            break;
+    }
+
+}
+
+
+function createNewLine() {
+    line_number++;
+    textEditor.innerHTML += `<br><span contentEditable="true" id="line${line_number}"></span>`
+    contentEditable = document.getElementById(`line${line_number}`);
+    contentEditable.addEventListener("keydown", keydownEventHandler);
+    contentEditable.focus()
 }
