e9ea6f1067678bf70185cbce2626bf22d1794316
roshmadosh <hiroshi.nobuoka@gmail.com>
2022-11-11 12:32:58 -0500

    bugfix: editor was expanding if contenteditable was too long.

--- static/index.js
+++ static/index.js
@@ -1,12 +1,16 @@
-const contentEditable = document.querySelector("[contenteditable]")
+let contentEditable = document.getElementById("line1")
 const placeholder = document.getElementById("placeholder");
 
-contentEditable.addEventListener("focusin", contentEditableEventHandler)
+
+let line_number = 1;
+
+contentEditable.addEventListener("focusin", contentEditableEventHandler);
 contentEditable.addEventListener("focusout", contentEditableEventHandler);
+contentEditable.addEventListener("keydown", contentEditableEventHandler);
+
 
 function contentEditableEventHandler(e) {
     const event = e.type
-
     switch (event) {
         case 'focusin':       
             if (!contentEditable.textContent) {
@@ -16,6 +20,21 @@ function contentEditableEventHandler(e) {
             if (!contentEditable.textContent) {
                 placeholder.classList.remove('invisible')
             } break;
+
+        case 'keydown':
+            const textEditor = document.getElementById('text-editor');
+            const max_width = textEditor.offsetWidth * .87;
+
+            if (contentEditable.offsetWidth > max_width || e.key === 'Enter') {
+                e.preventDefault();
+                line_number++
+                textEditor.innerHTML += `<br><span contentEditable="true" id="line${line_number}"></span>`
+                contentEditable = document.getElementById(`line${line_number}`);
+                contentEditable.addEventListener("keydown", contentEditableEventHandler);
+                contentEditable.focus()
+            }
+
+
         default:
             break;
         }
